ca5c4311c42aacddb13b60816e5b39c0a0ce68ce
Omkar-Kurade <omkarkurade14@gmail.com>
2026-02-22 18:49:57 +0530

    Added Websocked logic for join leave and chat

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,34 +1,94 @@
-
-import { WebSocketServer } from "ws";
-import jwt, { JwtPayload }  from "jsonwebtoken";
+import WebSocket, { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import { prisma } from "@repo/db";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws , request) {
-    const url = request.url;
-    if(!url){
+interface User {
+  ws: WebSocket;
+  rooms: string[];
+  userId: string;
+}
+
+const users: User[] = [];
+
+function checkUser(token: string): string | null {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (typeof decoded === "string") {
+      return null;
+    }
+
+    if (!decoded || !decoded.userId) {
+      return null;
+    }
+    return decoded.userId;
+  } catch (error) {
+    console.log("JWT error:", error);
+    return null;
+  }
+}
+
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  const userId = checkUser(token);
+
+  if (userId == null) {
+    ws.close();
+    return null;
+  }
+
+  users.push({
+    userId,
+    rooms: [],
+    ws,
+  });
+
+  ws.on("message", function message(data) {
+    let parsedData;
+    try {
+        parsedData  = JSON.parse(data as unknown as string);
+    } catch (e) {
+        console.log("Invalid JSON received:", e)
         return
     }
 
-    const queryParams = new URLSearchParams(url.split("?")[1])
-    const token = queryParams.get('token') || ""
-    const decoded = jwt.verify(token , JWT_SECRET)
-
-    if(typeof decoded == "string"){
-        ws.close()
-        return 
+    if (parsedData.type === "join_room") {
+      const user = users.find((x) => x.ws === ws);
+      user?.rooms.push(parsedData.roomId);
     }
 
- 
-    if(!decoded || decoded.userId){
-        ws.close();
-        return
+    if (parsedData.type === "leave_room") {
+      const user = users.find((x) => x.ws === ws);
+      if (!user) {
+        return;
+      } 
+      user.rooms = user?.rooms.filter((x) => x !== parsedData.roomId);
     }
- 
-    ws.on('message', function message(data) {
 
-        ws.send('pong');
-    });
+    if (parsedData.type === "chat") {
+      const roomId = parsedData.roomId;
+      const message = parsedData.message;
 
+      users.forEach((user) => {
+        if (user.rooms.includes(roomId)) {
+          user.ws.send(
+            JSON.stringify({
+              type: "chat",
+              message: message,
+              roomId,
+            }),
+          );
+        }
+      });
+    }
+  });
 });
